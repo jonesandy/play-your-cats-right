663cece0004b8a0581fa1a493b83b6a9fc15a037
Andy Jones <andytjones@icloud.com>
2020-02-27 15:03:42 +0000

    Feature - can call API and get back 5 cats.

--- src/services/apiCall.test.js
+++ src/services/apiCall.test.js
@@ -2,7 +2,13 @@ import { fetchCats } from './apiCall';
 
 describe('api gets called', () => {
   
-  it('calls API', ()=> {
+  it('calls API and gets 200 code', ()=> {
+    fetchCats().then(res => {
+      expect(res.status).toEqual(200);
+    })
+  });
+
+  it('calls API and gets 5 cats', ()=> {
     fetchCats().then(res => {
       expect(res.body.length).toEqual(5);
     })
