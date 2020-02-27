d5eaccb749f8ec6e20abe4ed573776d852b0a6b0
Andy Jones <andytjones@icloud.com>
2020-02-27 15:40:02 +0000

    Feature - can show cards of cats on screen.

--- src/components/card.js
+++ src/components/card.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import { Card, Image } from 'semantic-ui-react';
 
+const Contacts = ({ cat }) => {
+  return (
+    <Card >
+      <Image src={cat.url} wrapped ui={false} />
+      <Card.Content>
+        <Card.Header>{cat.breeds[0].name}</Card.Header>
+      </Card.Content>
+      <Card.Content extra>
+        <p>This cat has a rating of X</p>
+      </Card.Content>
+    </Card>
+  )
+};
+
+export default Contacts;
